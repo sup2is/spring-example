856f294d04cbaf6022215da0a57f0b85ffd3156f
sup2is <dev.sup2is@gmail.com>
2019-06-07 15:08:04 +0900

    example update

---
+++ websocket-1/src/main/java/com/sup2is/websocket/MyHandler.java
@@ -0,0 +1,17 @@
+package com.sup2is.websocket;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class MyHandler extends TextWebSocketHandler {
+
+	@Override
+	public void handleTextMessage(WebSocketSession session, TextMessage message) {
+
+		System.out.println(session.toString());
+		System.out.println(message.toString());
+		
+	}
+
+}
---
+++ websocket-1/src/main/java/com/sup2is/websocket/WebsocketExamApplication.java
@@ -0,0 +1,13 @@
+package com.sup2is.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketExamApplication.class, args);
+	}
+
+}
