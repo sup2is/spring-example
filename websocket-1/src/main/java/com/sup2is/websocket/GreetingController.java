93c9457955242273a337ef1f01fe5167a411e67b
sup2is <dev.sup2is@gmail.com>
2019-06-14 01:37:31 +0900

    example update

--- websocket-1/src/main/java/com/sup2is/websocket/GreetingController.java
+++ websocket-1/src/main/java/com/sup2is/websocket/GreetingController.java
@@ -1,16 +1,21 @@
 package com.sup2is.websocket;
 
-import java.util.Date;
-
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+import com.sup2is.websocket.model.Greeting;
+import com.sup2is.websocket.model.HelloMessage;
 
 @Controller
 public class GreetingController {
 
-	@MessageMapping("/greeting") 
-    public String handle(String greeting) {
-        return "[" + new Date(System.currentTimeMillis()) + ": " + greeting;
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
-	
+
 }
--- websocket-1/src/main/java/com/sup2is/websocket/WebSocketConfig.java
+++ websocket-1/src/main/java/com/sup2is/websocket/WebSocketConfig.java
@@ -6,20 +6,24 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
 @Configuration
-@EnableWebSocket
+@EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-	
-	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("example").withSockJS();
-	}
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
 
-	@Override
-	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		registry.setApplicationDestinationPrefixes("/app");
-		registry.enableSimpleBroker("/topic");
-	}
-	
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    }
 }
