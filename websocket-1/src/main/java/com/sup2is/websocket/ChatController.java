2d9ca02b1bf72c80447f8080ac236031fc978e13
sup2is <dev.sup2is@gmail.com>
2019-06-21 01:34:58 +0900

    example update

--- websocket-1/src/main/java/com/sup2is/websocket/ChatController.java
+++ websocket-1/src/main/java/com/sup2is/websocket/ChatController.java
@@ -1,7 +1,6 @@
 package com.sup2is.websocket;
 
-import java.security.Principal;
-
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -11,11 +10,11 @@ import com.sup2is.websocket.model.Greeting;
 import com.sup2is.websocket.model.HelloMessage;
 
 @Controller
-public class GreetingController {
+public class ChatController {
 
-    @MessageMapping("/hello")
-//    @SendTo("/topic/greetings")
-    public Greeting greeting(HelloMessage message , Principal user) throws Exception {
+    @MessageMapping("/chat/")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message, @Header("simpSessionId") String sessionId) throws Exception {
         Thread.sleep(1000); // simulated delay
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
--- websocket-1/src/main/java/com/sup2is/websocket/WebSocketConfig.java
+++ websocket-1/src/main/java/com/sup2is/websocket/WebSocketConfig.java
@@ -2,12 +2,6 @@ package com.sup2is.websocket;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -24,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+        registry.addEndpoint("/chat").withSockJS();
     }
 }
