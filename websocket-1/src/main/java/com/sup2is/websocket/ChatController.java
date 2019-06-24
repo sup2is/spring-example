2ea89fe949fe680a6bf647a8bd01b1a71649b6ef
sup2is <dev.sup2is@gmail.com>
2019-06-24 23:12:31 +0900

    example update

--- websocket-1/src/main/java/com/sup2is/websocket/ChatController.java
+++ websocket-1/src/main/java/com/sup2is/websocket/ChatController.java
@@ -3,6 +3,7 @@ package com.sup2is.websocket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
