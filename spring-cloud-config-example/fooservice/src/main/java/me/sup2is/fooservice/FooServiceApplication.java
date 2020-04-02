5a0dc8b6b2e215c24a2780d59f0a6099c32728e6
sup2is <dev.sup2is@gmail.com>
2020-04-02 15:15:44 +0900

    Spring Cloud Config Server로 Configuration 관리하기 예제 업로드

---
+++ spring-cloud-config-example/fooservice/src/main/java/me/sup2is/fooservice/FooServiceApplication.java
@@ -0,0 +1,12 @@
+package me.sup2is.fooservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FooServiceApplication.class, args);
+    }
+}
