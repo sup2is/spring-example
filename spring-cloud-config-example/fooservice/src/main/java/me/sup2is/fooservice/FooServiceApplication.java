4e7b3bd95783087b3f3e856b4aa5007b16369571
sup2is <dev.sup2is@gmail.com>
2020-04-16 17:05:22 +0900

    예제 수정

--- spring-cloud-config-example/fooservice/src/main/java/me/sup2is/fooservice/FooServiceApplication.java
+++ spring-cloud-config-example/fooservice/src/main/java/me/sup2is/fooservice/FooServiceApplication.java
@@ -1,12 +1,27 @@
 package me.sup2is.fooservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RefreshScope
 public class FooServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FooServiceApplication.class, args);
     }
+
+    @Value("${example.property}")
+    private String exampleProperty;
+
+    @GetMapping("/my-property")
+    public String getProperty() {
+        return exampleProperty;
+    }
+
 }
